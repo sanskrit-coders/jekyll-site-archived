45a3be31f66311bc8c9a3bb36dbf301fccb248a4
vvasuki <vishvas.vasuki@gmail.com>
2018-05-26 14:34:50 -0700

    Add placeholder files.

--- make_pages.sh
+++ make_pages.sh
@@ -1,18 +1,16 @@
-printf -- "---\ntitle: Fix title!\n---\n">pages/survey.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/blog/index.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/dicts.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/books.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/audio.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/apps/intro.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/apps/translations.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/apps/deep-reading.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/fonts.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/ocr/intro.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/ocr/corrections.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/law.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/wikis.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/content/offline-web.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/tips/license.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/tips/dev-tools.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/tips/py-unicode.md
-printf -- "---\ntitle: Fix title!\n---\n">pages/tools/astronomy.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/dicts.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/books.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/audio.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/apps/intro.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/apps/translations.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/apps/deep-reading.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/fonts.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/ocr/intro.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/ocr/corrections.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/law.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/wikis.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/content/offline-web.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/tips/license.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/tips/dev-tools.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/tips/py-unicode.md
+printf -- "---\ntitle: Fix title!\n---\n"> pages/astronomy.md
